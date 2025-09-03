2cabeb06990b829909e44aa57ce410ea67172aa2
CHELANGAT ANTHONY <chelangatanthony@gmail.com>
2025-09-03 14:24:58 +0300

    another

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise";
 const executeQuery = async (query, data) => {
   try {
     const db = await mysql.createConnection({
-      host: "7.tcp.eu.ngrok.io",
+      host: "0.tcp.in.ngrok.io",
       port: "18245",
       user: "root",
       password: "",
