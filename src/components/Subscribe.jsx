04fd5dabbe56d7d85972992f898751772cc370d5
CHELANGAT ANTHONY <chelangatanthony@gmail.com>
2025-09-09 17:57:21 +0300

    UI

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -2,7 +2,7 @@ import { addSubscriber } from "@/lib/data";
 import React from "react";
 const Subscribe = () => {
   return (
-    <div>
+    <div className="text-[white]">
       <h1 className="text-center text-lg">News Letter</h1>
       <p className="text-center text-sm mb-3">Subscribe to our newsletter</p>
       <form action={addSubscriber}>
@@ -13,7 +13,7 @@ const Subscribe = () => {
           placeholder="Enter your email..."
         />
         <input
-          className="py-2 px-4 outline-none b-0 bg-red-500"
+          className="py-2 px-4 text-sm outline-none b-0 bg-red-500"
           type="submit"
           placeholder="Submit"
         />
