7280effc88c197969936ad434c444acec31e7832
CHELANGAT ANTHONY <chelangatanthony@gmail.com>
2025-12-16 19:05:15 +0300

    Kayam nas

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -3,8 +3,8 @@ import mysql from "mysql2/promise";
 const executeQuery = async (query, data) => {
   try {
     const db = await mysql.createConnection({
-      host: "6.tcp.eu.ngrok.io",
-      port: "10308",
+      host: "4.tcp.eu.ngrok.io",
+      port: "13945",
       user: "root",
       password: "",
       database: "sipi",
