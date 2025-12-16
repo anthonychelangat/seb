0cd6af297d35b0ada2703834de9db3c5e853e40e
CHELANGAT ANTHONY <chelangatanthony@gmail.com>
2025-12-16 17:05:34 +0300

    Initial commit

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -3,8 +3,8 @@ import mysql from "mysql2/promise";
 const executeQuery = async (query, data) => {
   try {
     const db = await mysql.createConnection({
-      host: "127.0.0.1",
-      port: "3306",
+      host: "6.tcp.eu.ngrok.io",
+      port: "10308",
       user: "root",
       password: "",
       database: "sipi",
