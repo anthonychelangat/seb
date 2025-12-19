d9df26ba514da84168ae259c2920655cc39a1b12
CHELANGAT ANTHONY <chelangatanthony@gmail.com>
2025-12-19 14:47:00 +0300

    final

--- src/components/SessionHeader.jsx
+++ src/components/SessionHeader.jsx
@@ -4,30 +4,26 @@ import { getUserByEmail } from "@/lib/actions";
 import IntermediateHeader from "./IntermediateHeader";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]";
-import executeQuery from "@/lib/utils";
+//import executeQuery from "@/lib/utils";
 
 const SessionHeader = async () => {
   const session = await getServerSession(authOptions);
   const email = session?.user?.email ?? "tony@gmail.com";
-
   const users = await getUserByEmail(email);
 
-  if (session) {
-    const name = session?.user?.name ?? "tony";
+  //const existing = await executeQuery("select id from users where email=?", [
+  //  email
+  //]);
 
-    const existing = await executeQuery("select id from users where email=?", [
-      email,
-    ]);
+  //const role = 1;
 
-    const role = 1;
-
-    if (existing.length === 0) {
-      await executeQuery(
-        "insert into users(username,email,role) values(?,?,?)",
-        [name, email, role]
-      );
-    }
-  }
+  // if (existing.length === 0) {
+  //   await executeQuery("insert into users(username,email,role) values(?,?,?)", [
+  //    name,
+  //    email
+  //    role,
+  //  ]);
+  // }
 
   return (
     <div className="hidden md:block xl:block fixed left-0 right-0 w-[100%] z-100 top-0 h-[7rem]">
