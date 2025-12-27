b1998275cd314455aed36eb8c624706d38e088a7
CHELANGAT ANTHONY <chelangatanthony@gmail.com>
2025-12-27 17:44:15 +0300

    trial

--- src/components/SessionHeader.jsx
+++ src/components/SessionHeader.jsx
@@ -3,19 +3,20 @@ import { getUserByEmail } from "@/lib/actions";
 import IntermediateHeader from "./IntermediateHeader";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]";
-//import executeQuery from "@/lib/utils";
+import executeQuery from "@/lib/utils";
 
 const SessionHeader = async () => {
   const session = await getServerSession(authOptions);
   const email = session?.user?.email ?? "tony@gmail.com";
   const users = await getUserByEmail(email);
+  const name = session?.user?.name ?? "tony";
 
   if (users.length === 0) {
     const role = 3;
 
     await executeQuery("insert into users(username,email,role) values(?,?,?)", [
-      session.user.name,
-      session.user.email,
+      name,
+      email,
       role,
     ]);
   }
